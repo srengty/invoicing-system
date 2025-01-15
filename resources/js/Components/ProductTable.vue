88571f3bfc2130e34d870a9f08a6b13579c6fcc3
sreng.itc <sreng@itc.edu.kh>
2025-01-15 22:31:05 +0700

    add primevue

---
+++ resources/js/Components/ProductTable.vue
@@ -0,0 +1,42 @@
+
+<template>
+    <div class="card">
+        <DataTable :value="products" tableStyle="min-width: 50rem">
+            <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header"></Column>
+            <Column field="action" header="Action">
+                <template #body="slotProps">
+                    <img :src="`https://picsum.photos/seed/${slotProps.data.image}/300/150`" :alt="slotProps.data.image" class="w-24 rounded" />
+                </template>
+            </Column>
+        </DataTable>
+        <br/><Button>New</Button>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Button from 'primevue/button';
+//import { ProductService } from '@/service/ProductService';
+
+onMounted(() => {
+    products.value = [
+        { code: 'P001', name: 'Product 1', category: 'Category 1', quantity: 10, image: 'product1' },
+        { code: 'P002', name: 'Product 2', category: 'Category 2', quantity: 20, image: 'product2' },
+        { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30, image: 'product3' },
+        { code: 'P004', name: 'Product 4', category: 'Category 4', quantity: 40, image: 'product5' },
+        { code: 'P005', name: 'Product 5', category: 'Category 5', quantity: 50, image: 'product6' },
+    ];
+    //ProductService.getProductsMini().then((data) => (products.value = data));
+});
+
+const products = ref();
+const columns = [
+    { field: 'code', header: 'Code' },
+    { field: 'name', header: 'Name' },
+    { field: 'category', header: 'Category' },
+    { field: 'quantity', header: 'Quantity' }
+];
+
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,6 +21,11 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            })
             .mount(el);
     },
     progress: {
