e4d657cb9891634b29533cd1f2325a9e2089a193
Monopich <126034376+Monopich@users.noreply.github.com>
2025-03-03 14:50:39 +0700

    update at index customer

--- resources/js/Pages/Customers/Index.vue
+++ resources/js/Pages/Customers/Index.vue
@@ -1,17 +1,23 @@
 <template>
     <Head title="Customers/Organization Name" />
     <GuestLayout>
-        <div class="customers">
-            <div class="flex justify-between items-center p-3">
-                <h1 class="text-2xl">Customers/Organization Name</h1>
+        <BodyLayout>
+            <div class="customers">
+            <div class="flex justify-between items-center p-3 pr-0">
+                <h1 class="text-xl">Customers/Organization Name</h1>
                 <div>
                     <Link :href="route('customers.create')">
-                        <Button icon="pi pi-plus" label="New" rounded />
+                        <Button icon="pi pi-plus" label="New" rounded size="small"/>
                     </Link>
-                    <ChooseColumns :columns="columns" v-model="selectedColumns" @apply="updateColumns" rounded />
+                    <ChooseColumns :columns="columns" v-model="selectedColumns" @apply="updateColumns" rounded size="small"/>
                 </div>
             </div>
-            <DataTable :value="customers" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]">
+            <DataTable :value="indexedCustomers" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" class="text-sm">
+                <Column field="index" header="#" style="width: 50px">
+                    <template #body="slotProps">
+                        {{ slotProps.data.index }}
+                    </template>
+                </Column>
                 <Column v-for="col of showColumns" :key="col.field" :field="col.field" :header="col.header" sortable></Column>
                 <Column header="Actions">
                     <template #body="slotProps">
@@ -20,103 +26,92 @@
                                 icon="pi pi-eye"
                                 class="p-button-info"
                                 aria-label="View"
+                                size="small"
                                 @click="viewCustomer(slotProps.data.id)"
-                                rounded
+                                outlined
                             />
                             <Button
                                 icon="pi pi-pencil"
                                 class="p-button-warning"
                                 aria-label="Edit"
+                                size="small"
                                 @click="editCustomer(slotProps.data.id)"
-                                rounded
+                                outlined
                             />
                             <Button
                                 icon="pi pi-trash"
                                 class="p-button-danger"
                                 aria-label="Delete"
+                                size="small"
                                 @click="deleteCustomer(slotProps.data.id)"
-                                rounded
+                                outlined
                             />
                         </div>
                     </template>
                 </Column>
             </DataTable>
         </div>
+        </BodyLayout>
     </GuestLayout>
 </template>
 
-<script>
-import { ref } from "vue";
+<script setup>
+import { computed, ref } from 'vue'; // Import ref here
 import { Head, Link } from '@inertiajs/vue3';
 import { DataTable, Column, Button } from 'primevue';
 import ChooseColumns from '@/Components/ChooseColumns.vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
+import BodyLayout from '@/Layouts/BodyLayout.vue';
 
-export default {
-    components: {
-        Head,
-        Link,
-        DataTable,
-        Column,
-        Button,
-        ChooseColumns,
-        GuestLayout
-    },
-    props: {
-        customers: Array,
-    },
-    data() {
-        return {
-            columns: [
-                { field: 'id', header: 'ID' },
-                { field: 'name', header: 'Name' },
-                { field: 'code', header: 'Code' },
-                { field: 'email', header: 'Email' },
-                { field: 'address', header: 'Address' },
-                { field: 'phone_number', header: 'Phone' },
-                { field: 'telegram_number', header: 'Telegram' },
-                { field: 'website', header: 'Website' },
-                { field: 'bank_name', header: 'Bank Name' },
-                { field: 'bank_address', header: 'Bank Address' },
-                { field: 'bank_account_name', header: 'Bank Account Name' },
-                { field: 'bank_account_number', header: 'Bank Account Number' },
-                { field: 'bank_swift', header: 'Bank Swif' },
-            ],
-            selectedColumns: [],
-            showColumns: []
-        };
-    },
-    methods: {
-        updateColumns() {
-            this.showColumns = this.selectedColumns;
-        },
-        createCustomer() {
-            this.$inertia.visit(route('customers.create'));
-        },
-        editCustomer(id) {
-            // Corrected route for the edit form
-            this.$inertia.visit(route('customers.edit', id)); 
-        },
-        deleteCustomer(id) {
-            if (confirm('Are you sure you want to delete this customer?')) {
-                this.$inertia.delete(route('customers.destroy', id), {
-                    onSuccess: () => {
-                        // Optionally, handle success (e.g., refresh the customer list)
-                        console.log('Customer deleted!');
-                    },
-                    onError: (error) => {
-                        console.error('Error deleting customer:', error);
-                    }
-                });
+const indexedCustomers = computed(() => {
+    return props.customers.map((customer, index) => ({
+        ...customer,
+        index: index + 1, // Assigns index starting from 1
+    }));
+});
+
+const props = defineProps({
+    customers: Array,
+});
+
+const columns = [
+    { field: 'name', header: 'Name' },
+    { field: 'code', header: 'Code' },
+    { field: 'email', header: 'Email' },
+    { field: 'phone_number', header: 'Phone' },
+    { field: 'telegram_number', header: 'Telegram' },
+    { field: 'bank_swift', header: 'Bank Swift' },
+];
+
+const selectedColumns = ref([]);
+const showColumns = ref(columns);
+
+const updateColumns = () => {
+    showColumns.value = selectedColumns.value;
+};
+
+const createCustomer = () => {
+    Inertia.visit(route('customers.create'));
+};
+
+const editCustomer = (id) => {
+    Inertia.visit(route('customers.edit', id));
+};
+
+const deleteCustomer = (id) => {
+    if (confirm('Are you sure you want to delete this customer?')) {
+        Inertia.delete(route('customers.destroy', id), {
+            onSuccess: () => {
+                console.log('Customer deleted!');
+            },
+            onError: (error) => {
+                console.error('Error deleting customer:', error);
             }
-        },
-        viewCustomer(id) {
-            this.$inertia.visit(route('customers.show', id));
-        }
-    },
-    created() {
-        // Set the default columns to show
-        this.showColumns = this.columns;
+        });
     }
+};
+
+const viewCustomer = (id) => {
+    Inertia.visit(route('customers.show', id));
 };
 </script>
