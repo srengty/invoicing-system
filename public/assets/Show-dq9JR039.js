c2dfde1504b438ba36ad9e8817ed63b144ae1755
Goku <pacharothroeun01@gmail.com>
2025-04-03 16:55:14 +0700

    add template and script

---
+++ public/assets/Show-dq9JR039.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{d as o,e as r}from"./app-bSMdDCtv.js";const c={};function t(n,s){return r(),o("div")}const f=e(c,[["render",t]]);export{f as default};
---
+++ public/assets/index-Dw0KrZvb.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-Ds-6aC4Y.js";var t={name:"Dropdown",extends:e,mounted:function(){console.warn("Deprecated since v4. Use Select component instead.")}};export{t as s};
