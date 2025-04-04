1e2cf5854af7f2411f361338bc537c6179be34cb
Goku <pacharothroeun01@gmail.com>
2025-04-05 00:27:10 +0700

    change flow

--- vite.config.prod.js
+++ vite.config.prod.js
@@ -3,36 +3,31 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: 'https://finance.itc.edu.kh/',
+    base: '/build/',
     // build: {
     //     outDir: './public',
     //     emptyOutDir: false, // also necessary
     // },
-    server: {
-        hmr: {
-            host: 'finance.itc.edu.kh',
-        },
-        https: true,
-        cors: {
-            origin: [
-                'https://finance.itc.edu.kh',
-            ],
-
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
         },
     },
+    // server: {
+    //     hmr: {
+    //         host: 'finance.itc.edu.kh',
+    //     },
+    //     https: true,
+    //     cors: {
+    //         origin: [
+    //             'https://finance.itc.edu.kh',
+    //         ],
+
+    //     },
+    // },
     plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-            detectTls: 'finance.itc.edu.kh'
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(),
     ],
 });
