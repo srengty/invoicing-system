8d5f32b98afce5d1cd65f4497cb05b4f68686047
Goku <pacharothroeun01@gmail.com>
2025-04-05 08:18:34 +0700

    add server

--- vite.config.prod.js
+++ vite.config.prod.js
@@ -11,27 +11,28 @@ export default defineConfig({
     //     },
     // },
 
-    // server: {
-    //     hmr: {
-    //         host: 'finance.itc.edu.kh',
-    //     },
-    //     https: true,
-    //     cors: {
-    //         origin: [
-    //             'https://finance.itc.edu.kh',
-    //         ],
+    server: {
+        hmr: {
+            host: 'finance.itc.edu.kh',
+        },
+        https: true,
+        cors: {
+            origin: [
+                'https://finance.itc.edu.kh',
+            ],
 
-    //     },
-    // },
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
+            detectTls: 'finance.itc.edu.kh',
         }),
         vue({
             template: {
                 transformAssetUrls: {
-                    base: "/build/",
+                    base: null,
                     includeAbsolute: false,
                 },
             },
