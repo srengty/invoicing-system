b9d440aebfa20fafe1f5063b004d355ea5ce77c6
Goku <pacharothroeun01@gmail.com>
2025-04-05 13:58:53 +0700

    add route

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 window.axios = axios;
-window.axios.defaults.baseURL = import.meta.env.APP_URL;
+window.axios.defaults.baseURL = import.meta.env.APP_URL || 'https://finance.itc.edu.kh';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
