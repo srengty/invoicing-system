029937267360c49d6715b499672a3e358ca9a2aa
Pacharoth <53423403+Pacharoth@users.noreply.github.com>
2025-04-05 08:31:35 +0700

    Update vite.config.prod.js

--- vite.config.prod.js
+++ vite.config.prod.js
@@ -3,14 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    // build: {
-    //     outDir: './public/build',
-    //     manifest: true,
-    //     rollupOptions: {
-    //       input : "resources/js/app.js",
-    //     },
-    // },
-
     server: {
         hmr: {
             host: 'finance.itc.edu.kh',
