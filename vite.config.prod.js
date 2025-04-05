ff5062bd3fd8aa517a328a72b52ed47a36750284
Goku <pacharothroeun01@gmail.com>
2025-04-05 15:59:43 +0700

    another commit

--- vite.config.prod.js
+++ vite.config.prod.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-
+import ziggy from 'laravel-vite-plugin/ziggy';
 export default defineConfig({
     server: {
         hmr: {
@@ -29,5 +29,6 @@ export default defineConfig({
                 },
             },
         }),
+        ziggy()
     ],
 });
