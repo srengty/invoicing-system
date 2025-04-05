92623745242e60adb39df00e102e5247b53ea84d
Goku <pacharothroeun01@gmail.com>
2025-04-05 22:23:57 +0700

    add CSRF token for axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,4 @@ import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.baseURL = import.meta.env.APP_URL || 'https://finance.itc.edu.kh';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf_token"]').getAttribute('content');
