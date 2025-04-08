71b4d4f873424f9db3fdea79fb8389a96d7eff20
POMMOUYLANG <mouylangpom@gamail.com>
2025-04-08 21:40:39 +0700

    update sidebar

--- resources/js/Layouts/MainNav.vue
+++ resources/js/Layouts/MainNav.vue
@@ -22,52 +22,52 @@
     </div>
 </template>
 <script setup>
-import { Link, usePage } from '@inertiajs/vue3';
+import { Link, usePage } from "@inertiajs/vue3";
 const page = usePage();
 </script>
 <style>
 .main-nav-wrapper {
     @apply pl-8;
 }
-    .main-nav ol {
-        list-style: none;
-        counter-reset: muffins;
-    }
-    .main-nav ol li {
-        counter-increment: muffins;
-        line-height: 1.6rem;
-    }
-    .main-nav ol::before {
-        position: absolute;
-        top: 8px;
-        left: 0.6rem;
-        z-index: 1;
-        content: "\00a0";
-        width: 0.3rem;
-        height: 80%;
-        @apply bg-green-600;
-        margin-right: 8px;
-    }
-    .main-nav ol li::before {
-        content: "\00a0\00a0" counter(muffins);
-        color: white;
-        width: 1.5rem;
-        height: 1.5rem;
-        border-radius: 50%;
-        @apply bg-green-600;
-        margin-top: 0.25rem;
-        position: absolute;
-        z-index: 2;
-        left: 0px;
-    }
-    .main-nav ol li a {
-        line-height: 2rem;
-        padding-left: 2rem;
-    }
-    .main-nav ol li:hover {
-        @apply bg-green-100;
-    }
-    .main-nav ol li.active {
-        @apply bg-green-200;
-    }
+.main-nav ol {
+    list-style: none;
+    counter-reset: muffins;
+}
+.main-nav ol li {
+    counter-increment: muffins;
+    line-height: 1.6rem;
+}
+.main-nav ol::before {
+    position: absolute;
+    top: 8px;
+    left: 0.6rem;
+    z-index: 1;
+    content: "\00a0";
+    width: 0.3rem;
+    height: 80%;
+    @apply bg-green-600;
+    margin-right: 8px;
+}
+.main-nav ol li::before {
+    content: "\00a0\00a0"counter(muffins);
+    color: white;
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: 50%;
+    @apply bg-green-600;
+    margin-top: 0.25rem;
+    position: absolute;
+    z-index: 2;
+    left: 0px;
+}
+.main-nav ol li a {
+    line-height: 2rem;
+    padding-left: 2rem;
+}
+.main-nav ol li:hover {
+    @apply bg-green-100;
+}
+.main-nav ol li.active {
+    @apply bg-green-200;
+}
 </style>
