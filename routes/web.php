35974fea93029fdbcb4ed1aa6e8c1128747e5c39
Monopich <126034376+Monopich@users.noreply.github.com>
2025-04-10 16:13:24 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::get('/quotations/{quotation}/edit', [QuotationController::class, 'edit'])
 
 Route::resource('invoices', InvoiceController::class);
 Route::get('/invoices', [InvoiceController::class, 'index'])->name('invoices.index');
-Route::get('/invoices/show', [InvoiceController::class, 'show'])->name('invoices.show');
+Route::get('/invoices/create', [InvoiceController::class, 'create'])->name('invoices.create');
 // Route::get('/invoices/show', [InvoiceController::class, 'show'])->name('invoices.show');
 Route::get('/quotations/{quotation_no}/invoices', [InvoiceController::class, 'getInvoicesByQuotation']);
 
